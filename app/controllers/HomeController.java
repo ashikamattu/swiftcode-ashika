a266c87a763120cb195842b03a889db9c24c0e5e
ashikamattu <ashikamatt1@gmail.com>
2018-04-13 10:38:25 +0000

    Second Commit

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -18,11 +18,7 @@ import java.util.Objects;
 
 public class HomeController extends Controller {
     public Result chat(){
-        return ok("Hello World");
-
-
-
-
+        return ok("Hello World Ashika");
     }
 
 }
