7f58fc3c30c19ee0f35123658cfc53d47fdea17a
ashikamattu <ashikamatt1@gmail.com>
2018-04-14 12:12:34 +0000

    User-BotConvo

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,24 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Hi what an i do for you?'
+		},
+        {
+            'sender': 'USER',
+            'text': 'Order Pizza for me'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Give money'
+		}
+
+	];
 
 });
